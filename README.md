844215d4be6b0f24c9a8f291b54f1566180b5161
umodoc <team@umodoc.com>
2024-05-22 11:00:23 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,13 @@
+# Umo Editor 开发文档
+
+## 介绍
+
+本项目是 Umo Editor 所依赖的部分模块。
+
+Umo Editor 是一个基于 Vue3 的适合于国人使用的本土化开源文档编辑器。Umo Editor 提供完善的文档编辑能力，支持 Markdown 语法，支持基础的富文本编辑功能，支持多种插入多种格式的节点类型、提供了多种类型的实用工具，并支持设置页面的样式，支持导出多种类型的格式，支持页面预览和打印。
+
+[开发文档](https://editor.umodoc.com/docs) | [在线演示](https://demo.umodoc.com/editor) | [GitHub](https://github.com/umodoc/editor) | [码云(国内镜像)](https://gitee.com/umodoc/editor) | [NPM](https://www.npmjs.com/package/@umodoc/editor)
+
+## 开源协议
+
+Umo-Editor 源代码以 [MIT 许可证](https://opensource.org/licenses/MIT) 发布。
---
+++ build.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+
+// 删除 libs 目录
+if (fs.existsSync('libs')) {
+	fs.rmSync('libs', { recursive: true, force: true });
+}
+
+// 定义复制文件夹的函数
+const copyDir = (sourcePath, targetPath) => {
+	// 检查目标目录是否存在，如果不存在则创建
+	if (!fs.existsSync(targetPath)) {
+		fs.mkdirSync(targetPath, { recursive: true });
+	}
+
+	// 读取源目录中所有的条目
+	const entries = fs.readdirSync(sourcePath, { withFileTypes: true });
+
+	// 遍历所有条目
+	for (let entry of entries) {
+		const entryPath = path.join(sourcePath, entry.name);
+		const targetEntryPath = path.join(targetPath, entry.name);
+
+		if (entry.isDirectory()) {
+			// 如果是目录，则递归调用 copyDir
+			copyDir(entryPath, targetEntryPath);
+		} else {
+			// 如果是文件，则复制文件
+			fs.copyFileSync(entryPath, targetEntryPath);
+			console.log(`Copied file: ${entryPath} to ${targetEntryPath}`);
+		}
+	}
+};
+
+// 定义要复制的文件和目录列表
+const files = [
+	{ from: '@imgly/background-removal-data/dist', to: 'imgly/background-removal-data' },
+	{ from: 'monaco-editor/min', to: 'monaco-editor/min' },
+	{ from: 'katex/dist', to: 'katex' },
+	{ from: 'jsbarcode/dist/JsBarcode.all.min.js', to: 'jsbarcode.all.min.js' },
+	{ from: 'qrcode-svg/dist/qrcode.min.js', to: 'qrcode-svg.min.js' },
+	{ from: 'mermaid/dist/mermaid.min.js', to: 'mermaid.min.js' },
+	{ from: 'plyr/dist/plyr.min.js', to: 'plyr.min.js' },
+	{ from: 'pagedjs/dist/paged.polyfill.min.js', to: 'paged.polyfill.min.js' },
+	{ from: 'vue/dist/vue.global.prod.js', to: 'vue.min.js' }
+];
+
+// 循环遍历每个要复制的文件或目录
+files.forEach((item) => {
+	const sourcePath = path.join(__dirname, 'node_modules', item.from);
+	const targetPath = path.join(__dirname, 'libs', item.to);
+
+	// 如果是文件，使用fs.copyFileSync
+	if (fs.existsSync(sourcePath) && fs.statSync(sourcePath).isFile()) {
+		fs.copyFileSync(sourcePath, targetPath);
+		console.log(`Copied file: ${sourcePath} to ${targetPath}`);
+	}
+	// 如果是文件夹，调用递归复制函数
+	else {
+		copyDir(sourcePath, targetPath);
+		console.log(`Copied directory: ${sourcePath} to ${targetPath}`);
+	}
+});
